e68b224cffac414c24386d4248d8a3a7e329071c
Mick Hill - MacBook Pro <mickhill@hotmail.com.br>
2020-03-25 23:24:20 -0300

    Criacao de Feature

---
+++ learning-specflow/GooglePesquisaSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace learning_specflow
+{
+    [Binding]
+    public class GooglePesquisaSteps
+    {
+        [Given(@"que eu esteja na pagina inicial do google")]
+        public void DadoQueEuEstejaNaPaginaInicialDoGoogle()
+        {
+        }
+        
+        [When(@"eu pesquisar por um assunto")]
+        public void QuandoEuPesquisarPorUmAssunto()
+        {
+        }
+        
+        [When(@"eu pesquisar sem preencher o assunto")]
+        public void QuandoEuPesquisarSemPreencherOAssunto()
+        {
+        }
+        
+        [Then(@"me retorna os resultados indexados")]
+        public void EntaoMeRetornaOsResultadosIndexados()
+        {
+        }
+        
+        [Then(@"continuarei na mesma pagian aguardando um assunto")]
+        public void EntaoContinuareiNaMesmaPagianAguardandoUmAssunto()
+        {
+        }
+    }
+}
