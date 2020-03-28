5acacf966798e04450a7f94daf8100230b286636
Mick Hill - MacBook Pro <mickhill@hotmail.com.br>
2020-03-27 21:51:59 -0300

    Adicionando ChromeHeadless
    Executar sem exibir o browser

--- learning-specflow/DriverFactory.cs
+++ learning-specflow/DriverFactory.cs
@@ -2,6 +2,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System;
+using System.Collections.Generic;
 
 namespace learning_specflow
 {
@@ -10,7 +11,7 @@ namespace learning_specflow
 
         public static IWebDriver GetBrownser()
         {
-            string browserConfig = "Chrome";
+            string browserConfig = "ChromeHeadless";
             IWebDriver Browser;
 
             switch (browserConfig)
@@ -18,12 +19,31 @@ namespace learning_specflow
                 case "Chrome":
                     Browser = new ChromeDriver();
                     break;
+
+                case "ChromeHeadless":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArguments(
+                        new List<string>()
+                        {
+                            "--silent-launch",
+                            "--no-startup-window",
+                            "no-sandbox",
+                            "headless",
+                        }
+                    );
+                    ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
+                    chromeDriverService.HideCommandPromptWindow = true;
+                    Browser = new ChromeDriver(chromeDriverService, chromeOptions);
+                    break;
+
                 case "Firefox":
                     Browser = new FirefoxDriver();
                     break;
+
                 case "IE":
                     //Browser = new InternetExplorerDriver();
                     //break;
+
                 default:
                     throw new NotSupportedException("Browser Nao Suportado");
             }
